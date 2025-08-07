adb4b7c6b54583ced4dc00db289c15d75bcf3f74
Shahab Cypher <shahabcypher@gmail.com>
2025-08-07 08:50:31 +0330

    built check otp form structure

--- frontend/src/components/templates/CheckOtpForm.jsx
+++ frontend/src/components/templates/CheckOtpForm.jsx
@@ -1,5 +1,24 @@
-const CheckOtpForm = () => {
-  return <div>CheckOtpForm</div>;
+const CheckOtpForm = ({ otp, setOtp, mobile, setStep }) => {
+  const submitHandler = async (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>کد پیامک‌شده به شمارۀ «{mobile}» را وارد کنید.</span>
+      <label htmlFor="input">کد تأیید را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="کد تأیید"
+        value={otp}
+        onChange={(e) => setOtp(e.target.value)}
+      />
+      <button type="submit">ورود</button>
+      <button onClick={() => setStep(1)}>تغییر شماره موبایل</button>
+    </form>
+  );
 };
 
 export default CheckOtpForm;
--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -4,7 +4,7 @@ import CheckOtpForm from "../components/templates/CheckOtpForm";
 import SendOtpForm from "../components/templates/SendOtpForm";
 
 const AuthPage = () => {
-  const [step, setStep] = useState(1);
+  const [step, setStep] = useState(2);
   const [mobile, setMobile] = useState("");
   const [otp, setOtp] = useState("");
 
@@ -13,7 +13,14 @@ const AuthPage = () => {
       {step === 1 && (
         <SendOtpForm setStep={setStep} mobile={mobile} setMobile={setMobile} />
       )}
-      {step === 2 && <CheckOtpForm />}
+      {step === 2 && (
+        <CheckOtpForm
+          otp={otp}
+          setOtp={setOtp}
+          mobile={mobile}
+          setStep={setStep}
+        />
+      )}
     </div>
   );
 };
