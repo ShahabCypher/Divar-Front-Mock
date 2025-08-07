d0509f8308bd98af583405456f98aaaed4460abf
Shahab Cypher <shahabcypher@gmail.com>
2025-08-07 09:58:54 +0330

    configed react query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
+
 import Router from "router/Router";
+import defaultOptions from "configs/ReactQuery";
 
 function App() {
+  const queryClient = new QueryClient({ defaultOptions });
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
---
+++ frontend/src/configs/ReactQuery.js
@@ -0,0 +1,12 @@
+const defaultOptions = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      retry: false,
+      staleTime: 60 * 1000,
+    },
+  },
+};
+
+export default defaultOptions;
