6639e5f8069f4dd589c26c8483b06c18e8b6cfee
Shahab Cypher <shahabcypher@gmail.com>
2025-08-07 08:27:48 +0330

    built send otp form structure

--- frontend/src/components/templates/SendOtpForm.jsx
+++ frontend/src/components/templates/SendOtpForm.jsx
@@ -1,5 +1,26 @@
-const SendOtpForm = () => {
-  return <div>SendOtpForm</div>;
+const SendOtpForm = ({ setStep, mobile, setMobile }) => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <p>ورود به حساب کاربری</p>
+      <span>
+        برای استفاده از امکانات دیوار، لطفاً شمارهٔ موبایل خود را وارد کنید. کد
+        تأیید به این شماره پیامک خواهد شد.
+      </span>
+      <label htmlFor="input">شمارهٔ موبایل خود را وارد کنید</label>
+      <input
+        type="text"
+        id="input"
+        placeholder="شماره موبایل"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <button type="submit">ارسال کد تایید</button>
+    </form>
+  );
 };
 
 export default SendOtpForm;
