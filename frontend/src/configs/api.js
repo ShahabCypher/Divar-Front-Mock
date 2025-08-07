a83a5d629cf4abf9953364788712d90481855ce1
Shahab Cypher <shahabcypher@gmail.com>
2025-08-07 10:53:04 +0330

    moved set access token to an interceptor

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { getCookie } from "src/utils/cookie";
+import { getNewTokens } from "src/services/token";
+import { getCookie, setCookie } from "src/utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
@@ -18,6 +19,24 @@ api.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const res = await getNewTokens();
+      if (!res?.response) return;
+      setCookie(res.response.data);
+
+      return api(originalRequest);
+    }
   }
 );
 
