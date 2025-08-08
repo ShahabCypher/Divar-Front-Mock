5034b7e814d99419b565ce87365f8572058d0927
Shahab Cypher <shahabcypher@gmail.com>
2025-08-09 02:49:34 +0330

    showing posts in main page

--- frontend/src/components/templates/Main.jsx
+++ frontend/src/components/templates/Main.jsx
@@ -1,5 +1,24 @@
+import { sp } from "src/utils/numbers";
+
 const Main = ({ posts }) => {
-  return <div>Main</div>;
+  const URL = import.meta.env.VITE_BACKEND_URL;
+
+  return (
+    <div>
+      {posts.data.posts.map((post) => (
+        <div key={post._id}>
+          <div>
+            <p>{post.options.title}</p>
+            <div>
+              <p>{sp(post?.amount)} تومان</p>
+              <span>{post.options.city}</span>
+            </div>
+          </div>
+          <img src={`${URL}${post.images[0]}`} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Main;
