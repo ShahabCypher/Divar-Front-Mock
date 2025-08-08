5165a48b50816e32a9b4bb6d0ef011d424424ba4
Shahab Cypher <shahabcypher@gmail.com>
2025-08-08 20:23:20 +0330

    added delete button

--- frontend/src/components/templates/CategoryList.jsx
+++ frontend/src/components/templates/CategoryList.jsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 
 import Loader from "../modules/Loader";
-import { getCategories } from "services/admin";
+import { getCategories, deleteCategory } from "services/admin";
 
 import styles from "./CategoryList.module.css";
 
 const CategoryList = () => {
+  const [disabledButtons, setDisabledButtons] = useState({});
   const { data, isPending } = useQuery({
     queryKey: ["get-categories"],
     queryFn: getCategories,
   });
+
+  const deleteHandler = (id) => {
+    setDisabledButtons((prev) => ({ ...prev, [id]: true }));
+    deleteCategory(id);
+  };
 
   return (
     <div className={styles.list}>
@@ -17,10 +24,16 @@ const CategoryList = () => {
         <Loader />
       ) : (
         data.data.map((i) => (
-          <div key={i.id}>
+          <div key={i._id}>
             <img src={`${i.icon}.svg`} />
             <h5>{i.name}</h5>
             <p>slug: {i.slug}</p>
+            <button
+              onClick={() => deleteHandler(i._id)}
+              disabled={disabledButtons[i._id]}
+            >
+              حذف
+            </button>
           </div>
         ))
       )}
